55d989a63299e86ca43b18291c6643a1df40db1f
Max Xu <maxxu123456@gmail.com>
2024-03-04 21:54:40 -0500

    added api feature so you can get text from webpage

--- AI_Flask/AIBackend.py
+++ AI_Flask/AIBackend.py
@@ -1,11 +1,23 @@
+from urllib.parse import unquote
+
 from flask import Flask
+from text import get_text
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
     return "Hello World!"
 
 
-if __name__ == '__main__':
-    app.run() 
+@app.route("/textof/<path:url>")
+def text(url: str):
+    # url is encoded
+    decoded_url = unquote(url)
+
+    return get_text(decoded_url)
+
+
+if __name__ == "__main__":
+    app.run()
